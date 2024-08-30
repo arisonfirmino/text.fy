05f4d250b1f2616bda663557c814e8efa0e876fa
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-08-30 17:34:09 -0300

    feat: adiciona estado de carregamento ao DeleteButton com ícone de loader

--- app/components/delete-post.tsx
+++ app/components/delete-post.tsx
@@ -1,24 +1,39 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Trash2Icon } from "lucide-react";
+import { LoaderCircleIcon, Trash2Icon } from "lucide-react";
 import { deletePost } from "../actions/post";
 
 export default function DeleteButton({ id }: { id: string }) {
   const router = useRouter();
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleDeleteClick = async () => {
-    await deletePost({ id }).then(() => {
+    setIsLoading(true);
+
+    try {
+      await deletePost({ id });
+    } catch (error) {
+      console.error("Failed to delete the post", error);
+    } finally {
+      setIsLoading(false);
       router.push("/");
-    });
+    }
   };
 
   return (
     <button
       onClick={handleDeleteClick}
+      disabled={isLoading}
       className="absolute right-1 top-1 rounded-full bg-red-300 p-1 text-xs text-red-600 active:bg-black active:text-white"
     >
-      <Trash2Icon size={12} />
+      {isLoading ? (
+        <LoaderCircleIcon size={16} className="animate-spin" />
+      ) : (
+        <Trash2Icon size={12} />
+      )}
     </button>
   );
 }
